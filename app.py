5006c302d06f51e848990ed5dd2866b71fc6734b
MdsierraDeveloper <86983576+MdsierraDeveloper@users.noreply.github.com>
2021-10-06 21:12:16 -0500

    Actualizacion 2
    Actualizacion de rutas y plantillas basicas en templates y en app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+#from flask import Flask
+#from formularios.fomularios import Login, Register
+#aqui se instancian las librerias
+
+#se hacen las rutas
+@app.route("/")
+@app.route("/login/")
+def Login():
+    return "Pagina De LOgin"
+@app.route("/registro/")
+def Registro():
+    return "pagina de registro"
+@app.route("/Principal/")
+def inicio():
+    return "Pagina Feed"
+@app.route("/perfil/")
+def Perfil():
+    return "Pagina Perfil"
+@app.route("/admin/")
+def inicio():
+    return "Pagina Admin"
+
+
+
+
+
---
+++ formularios/fomularios.py
@@ -0,0 +1,15 @@
+#tenemos que descargar todas las librerias
+#from flask_wtf import FlaskForm
+#from wtforms import SelectField, IntegerField, TextAreaField, SubmitField
+#from wtforms.validators import DataRequired, NumberRange
+
+class Login():#recibe el formulario
+    contra = ""
+    email = ""
+
+class Register():
+    #todo lo que lleva registro
+    nombre = ""#se instancian las cualidades de cada Field
+    apellidos = ""
+    telefono = ""
+    email = ""
