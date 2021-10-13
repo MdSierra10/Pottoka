f057b2a69f3c117ef63ccbe3d5e815af96ba1bb1
OscarMenaRojas <91097827+OscarMenaRojas@users.noreply.github.com>
2021-10-13 15:20:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,35 +5,31 @@ from formularios.formularios import Login, Register
 #se instancia un objeto de flask para crear las rutas
 app=Flask(__name__)
 #se hacen las rutas y se colocan los metodos
-@app.route("/")
+
+@app.route("/home/")
 def home():
     return render_template("base.html")
 
-@app.route("/login/", methods=["GET","POST"])
+@app.route("/")
+@app.route("/login/", methods=["POST"])
 def Login():
     return render_template("login.html")
 
-@app.route("/registro/" , methods=["GET","POST"])
+@app.route("/registro/" , methods=["POST"])
 def Registro():
     return "pagina de registro"
 
-@app.route("/admin/", methods=["POST"])
+@app.route("/home/admin/", methods=["POST"])
 def dashboard():
     return "Pagina Admin"
 
-@app.route("/admin/estadistic", methods=["POST"])
-def dashboardEstadistic():
-    return"Nada"
-
-@app.route("/perfil/", methods=["GET"])
+@app.route("/home/perfil/", methods=["GET"])
 def Perfil():
     return "Pagina Perfil"
-
 
 @app.errorhandler(404)
 def page_not_found(error):
     return 'Página no encontrada...', 404
-
 
 if __name__ == '__main__':
    	app.run()
