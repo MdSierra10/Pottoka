f4153d9336e3dd59a70f398e367b50df4040135f
MdsierraDeveloper <86983576+MdsierraDeveloper@users.noreply.github.com>
2021-10-13 22:02:00 -0500

    Prueba de pull.actualizacion
    Prueba

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 from formularios.formularios import Login, Register
+
 #aqui se instancian las librerias
 
 #se instancia un objeto de flask para crear las rutas
 app=Flask(__name__)
 #se hacen las rutas y se colocan los metodos
+
+@app.route("/homero/")
+def homero():
+    return ("Prueba de pull")
 
 @app.route("/home/")
 def home():
