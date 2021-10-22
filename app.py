954a4b519dc50ae1a6f21e3efd40ce2801081cb1
MdsierraDeveloper <91982060+MdSierra10@users.noreply.github.com>
2021-10-21 22:23:05 -0500

    Jinja2 en los templates
    Se aplica la plantilla de jinja en los templates login y registro para poder loguearse correctamente, aun falta la correcta conexion a la base de datos.

--- app.py
+++ app.py
@@ -1,24 +1,60 @@
+import sqlite3
+import hashlib
+import os 
+from sqlite3.dbapi2 import Cursor
 from flask import Flask, render_template
+from werkzeug.wrappers import CommonRequestDescriptorsMixin
 from formularios.formularios import Login, Register
 
 #aqui se instancian las librerias
 #cambio de nombre de archivo a formularios.py
 #se instancia un objeto de flask para crear las rutas
 app=Flask(__name__)
+app.secret_key = os.urandom(20)
 #se hacen las rutas y se colocan los metodosr
 
-@app.route("/home/")
+@app.route("/home/", methods=["POST"])
 def home():
     return render_template("base.html")
 
 @app.route("/")
 @app.route("/login/", methods=["GET", "POST"])
-def Login():
-    return render_template("login.html")
+def loguearse():
+    frm = Login()
+    if frm.validate_on_submit():
+        email = frm.email.data
+        contraseña = frm.contraseña.data
+        #Aqui se aplica el hash para cifrar la contraseña
+        enc = hashlib.sha256(contraseña.encode())
+        pass_enc = enc.hexdigest()
+
+        with sqlite3.connect("pottoka.db") as con:
+            cursor = con.cursor()
+            cursor.execute("SELECT email FROM usuario = ? AND contraseña = ?", [email, pass_enc])
+        if cursor.fetchone():
+            return render_template("base.html")
+        else:
+            return "PASSWORD/CORREO ERRADOS"
+    return render_template("login.html", frm = frm)
 
 @app.route("/registro/" , methods=["GET", "POST"])
 def Registro():
-    return render_template("registro.html")
+    frm = Register()
+    if frm.validate_on_submit():
+        nombre = frm.nombre.data
+        correo = frm.correo.data
+        contraseña = frm.contraseña.data
+        telefono = frm.telefono.data
+        usuario = frm.usuario.data
+        enc = hashlib.sha256(contraseña.encode())
+        pass_enc = enc.hexdigest()
+        
+        #conexion a base de datos de registro
+        with sqlite3.connect("pottoka.db") as con:
+            cursor = con.cursor()#para manipular la base de datos
+            cursor.execute("INSERT INTO registro (nombre, email, contraseña, telefono, usuario) VALUES (?,?,?,?,?", 
+            [nombre, correo, contraseña, telefono, usuario ])
+    return render_template("registro.html", frm = frm )
 
 @app.route("/home/admin/", methods=["GET", "POST"])
 def dashboard():
--- formularios/formularios.py
+++ formularios/formularios.py
@@ -1,16 +1,23 @@
 #tenemos que descargar todas las librerias
 from flask_wtf import FlaskForm
-from wtforms import   SubmitField
-#from wtforms.validators import DataRequired, NumberRange
+from wtforms import SubmitField, PasswordField, StringField
+from wtforms.validators import DataRequired
 #cambio de nombre
-class Login():#recibe el formulario
-    contra = ()
-    email = ()
-    inicio = SubmitField("Iniciar Sesion")
+class Login(FlaskForm):#recibe el formulario
+    email  = StringField("Email", validators=[DataRequired("Correo es Obligatorio")], 
+    render_kw={"placeholder": "Email"})
 
-class Register():
+    contraseña = PasswordField("Contraseña", validators=[DataRequired("Password es obligatorio")],
+    render_kw={"placeholder": "Password"})
+
+    inicio = SubmitField("Iniciar Sesion", render_kw={"placeholder": "Iniciar sesión"})
+     
+
+class Register(FlaskForm):
     #todo lo que lleva registro
-    nombre = ""#se instancian las cualidades de cada Field
-    apellidos = ""
-    telefono = ""
-    registro = SubmitField("Registrarse")
+    nombre = StringField("Nombre", render_kw={"placeholder": "Nombre"})
+    correo = StringField("Correo", render_kw={"placeholder": "Correo"})
+    contraseña = PasswordField("Contraseña", render_kw={"placeholder": "Contraseña"})
+    telefono = StringField("Celular", render_kw={"placeholder": "Telefono"})
+    registrar = SubmitField("Registrar")
+    usuario = StringField("Usuario", render_kw={"placeholder": "Usuario"})
